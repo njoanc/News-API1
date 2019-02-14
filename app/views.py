50170bd71ceb58b48aeb2a58803c93c7244262d7
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-14 10:48:38 +0200

    adding variable blocks

--- app/views.py
+++ app/views.py
@@ -4,7 +4,10 @@ from app import app
 # Views
 @app.route('/')
 def index():
+
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    message = 'Welcome to the News Articles'
+    return render_template('index.html',message = message)
