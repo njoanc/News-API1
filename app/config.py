ae1646362cbcd2d2757ed34c8f5292925e1763f5
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-14 19:30:45 +0200

    fixing errors

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    'https://newsapi.org/v2/everything?{q=bitcoin}&from=2019-01-14&sortBy=publishedAt&apiKey={d1586deac05c4d1fbf9742533adf9dea}'
+    'https://newsapi.org/v2/everything?q=bitcoin&from=2019-01-14&sortBy=publishedAt&apiKey=d1586deac05c4d1fbf9742533adf9dea'
     pass
 
 class ProdConfig(Config):
