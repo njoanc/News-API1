684e2026de22a1e6b6e273a2a93f6137e7c08893
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-14 12:12:35 +0200

    adding control flows

--- app/views.py
+++ app/views.py
@@ -3,9 +3,11 @@ from app import app
 
 # Views
 @app.route('/news/<int:news_id>')
-def news(news_id):
+def index():
 
     '''
-    View news page function that returns the news details page and its data
+    View root page function that returns the index page and its data
     '''
-    return render_template('news.html',id = news_id)
+
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title)
