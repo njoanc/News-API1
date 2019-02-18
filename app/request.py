e15066323cbe9ce1433277888f4a331ad24aa80b
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-18 14:54:15 +0200

    unittests

--- app/request.py
+++ app/request.py
@@ -3,14 +3,21 @@ from app import app
 import urllib.request,json
 from .models import news
 
+
+
 News = news.News
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+api_key = None
 
 
 # Getting the news base url
-base_url = app.config["NEWS_API_BASE_URL"]
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
 
 def get_news(title):
         
