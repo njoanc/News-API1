28c195e6a6bca56522289c14df3f668d59c63510
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-18 12:46:16 +0200

    implementing a search functionality

--- app/request.py
+++ app/request.py
@@ -65,3 +65,19 @@ def process_articles(news_list):
                 news_articles.append(news_object)
 
         return news_articles 
+
+def search_news(news_title):
+    search_news_url = 'https://newsapi.org/v2/search/news?api_key={}&query={}'.format(api_key,news_title)
+
+    with urllib.request.urlopen(search_news_url) as url:
+        search_news_data = url.read()
+        search_news_response = json.loads(search_news_data)
+
+        search_news_articles = None
+
+        if search_news_response['articles']:
+            search_news_list = search_news_response['articles']
+            search_news_articles = process_articles(search_news_list)
+
+
+    return search_news_articles
