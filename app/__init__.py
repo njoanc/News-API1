697630a17b6460b95655af0bfcb8287f8bc443c8
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-18 14:04:25 +0200

    registering a Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,27 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+# # Initializing application
+# app = Flask(__name__,instance_relative_config = True)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
-from app import views
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
+# from app import views
--- app/views.py
+++ app/views.py
@@ -79,3 +79,18 @@ def news(title):
     reviews = Review.get_reviews(news.title)
 
     return render_template('news.html',title = title,news = news,reviews = reviews)
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
