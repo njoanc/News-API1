c33ba63d69d979a41d0b1ed104d007c158172cb9
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-18 10:26:06 +0200

    processing results in the request.py file

--- app/request.py
+++ app/request.py
@@ -33,3 +33,29 @@ def get_news(category):
 
 
     return news_articles
+
+   def process_articles(news_list):
+    '''
+    Function  that processes the news articles and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_articles: A list of news objects
+    '''
+    news_articles = []
+    for news_item in news_list:
+        author = news_item.get('author')    
+        title = news_item.get('title')
+        description = movie_item.get('description')
+        url = movie_item.get('url')
+        urlToImage = movie_item.get('urlToImage')
+        publishedAt = movie_item.get('publishedAt')
+        content = news_item.get('content')
+
+        if title:
+            new_object = News(author,title,description,url,urlToImage,publishedAt,content)
+            movie_articles.append(news_object)
+
+    return news_results 
