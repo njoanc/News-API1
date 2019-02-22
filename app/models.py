7b649161b6896b7020bf0450e8bdecb57ebb966a
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-22 19:03:37 +0200

    fixing error in @property

--- app/models.py
+++ app/models.py
@@ -77,16 +77,16 @@ class Role(db.Model):
     def __repr__(self):
         return f'User {self.name}'
 
-pass_secure  = db.Column(db.String(255))
+    pass_secure  = db.Column(db.String(255))
 
-    @property
-    def password(self):
+@property
+def password(self):
         raise AttributeError('You cannot read the password attribute')
 
-    @password.setter
-    def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
 
 
-    def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
