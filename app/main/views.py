e9a3b6d00d4c33322540a128c0e15604ccadf8a8
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-27 12:55:20 +0200

    adding and displaying the pitches category

--- app/main/views.py
+++ app/main/views.py
@@ -1,71 +1,35 @@
-from flask import render_template,request,redirect,url_for,abort
+from flask import render_template, request, redirect, url_for, abort
 from . import main
-from ..models import User
-# from .request import get_news,search_news, get_sources
-# from .models import reviews
-from .forms import ReviewForm,UpdateProfile
-from flask_login import login_required
+from ..models import User, Pitch, Comment
+from .forms import PitchForm, UpdateProfile, CommentsForm
+from flask_login import login_required, current_user
+from .. import db, photos
 
-
-# Review = reviews.Review
-
-# Views
 @main.route('/')
+@login_required
 def index():
-    title = 'Home - Welcome to the Pitches Website'
-    return render_template('index.html', title = title)
+    """
+    View root page function that returns the index page and its data
+    """
+    title = 'Pitches'
+    product_pitch = Pitch.query.filter_by(category = 'Product Pitch').all()
+    pickup_lines = Pitch.query.filter_by(category = 'Pickup Lines').all()
+    interview_pitch = Pitch.query.filter_by(category = 'Interview Pitch').all()
+    promotion_pitch = Pitch.query.filter_by(category = 'Promotion Pitch').all()
 
-
-# @app.route('/news/<title>')
-# def news(title):
-
-#     '''
-#     View news page function that returns the news details page and its data
-#     '''
-#     news = get_news(title)
-
-#     title = f'{title}'
-   
-
-#     return render_template('news.html',title = title,news=news)
-
-# @app.route('/search/<news_author>')
-# def search(news_author):
-#     '''
-#     View function to display the search articles
-#     '''
-#     news_author_list = news_author.split(" ")
-#     news_author_format = "+".join(news_author_list)
-#     searched_news = search_news(news_author_format)
-#     title = f'search articles for {news_author}'
-#     return render_template('search.html',news = searched_news)
-
-# @app.route('/news/review/new/<author>', methods = ['GET','POST'])
-# def new_review(author):
-    
-#     form = ReviewForm()
-#     news = get_news(author)
-
-#     if form.validate_on_submit():
-#         title = form.title.data
-#         review = form.review.data
-#         new_review = Review(news.author,title,news.urlToImage,review)
-#         new_review.save_review()
-#         return redirect(url_for('news',title = news.title ))
-
-#     title = f'{news.title} review'
-#     return render_template('new_review.html',title = title, review_form=form, news=news)
+    return render_template('index.html', title = title,product_pitch = product_pitch, pickup_lines = pickup_lines, interview_pitch = interview_pitch, promotion_pitch = promotion_pitch)
 
 @main.route('/user/<uname>')
+@login_required
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
 
     if user is None:
         abort(404)
 
-    return render_template("profile/profile.html", user=user)
+    return render_template('profile/profile.html', user = user)
 
-@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@main.route('/user/<uname>/update', methods = ['GET', 'POST'])
 @login_required
 def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
@@ -80,6 +44,51 @@ def update_profile(uname):
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for('main.profile',uname=user.username))
+        return redirect(url_for('.profile', uname = user.username))
 
-    return render_template('profile/update.html',form =form)
+    return render_template('profile/update.html', form = form)
+
+@main.route('/user/<uname>/update/pic', methods = ['POST'])
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+
+    return redirect(url_for('main.profile', uname = uname))
+
+@main.route('/pitch/new_pitch', methods = ['GET','POST'])
+@login_required
+def new_pitch():
+    pitch_form = PitchForm()    
+
+    if pitch_form.validate_on_submit():
+        pitch = Pitch(title = pitch_form.title.data, category = pitch_form.category.data, pitch_content = pitch_form.pitch_content.data, author = pitch_form.author.data)
+
+        db.session.add(pitch)
+        db.session.commit() 
+        
+        return redirect(url_for('main.index'))
+    
+    return render_template('new_pitch.html', pitch_form = pitch_form)   
+
+@main.route('/pitch/comments', methods = ['GET', 'POST'])
+@login_required
+def comments():    
+    comments_form = CommentsForm() 
+    comments = Comment.query.all()    
+
+    if comments_form.validate_on_submit():       
+
+        # Updated comment instance
+        new_comment = Comment(body = comments_form.body.data)
+
+        # Save review method
+        new_comment.save_comment()
+
+        return redirect(url_for('main.comments'))
+    
+    return render_template('comments.html', comments_form = comments_form, comments = comments)
