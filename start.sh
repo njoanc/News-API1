138d06254b0b634b14e018f0f12906991a25abbf
njoanc <45597378+njoanc@users.noreply.github.com>
2019-03-06 15:15:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 # export MOVIE_API_KEY=d1586deac05c4d1fbf9742533adf9dea
 export SECRET_KEY='kazuba'
-export MAIL_USERNAME= njoanc@gmail.com
-export MAIL_PASSWORD= kazubajoanna
+export MAIL_USERNAME='njoanc@gmail.com'
+export MAIL_PASSWORD='kazubajoanna'
 export DATABASE_URL ='postgresql+psycopg2://wecode:kazuba1@localhost/pitch'
 
 python3.6 manage.py server
+
+
 
 
